731d3f84a939268b3eccceaf92468d6397da088c
ayoade olamilekan <ayoadelmlkn@gmail.com>
2026-06-21 22:54:33 +0100

    update joyforth2 corrections

--- site-config.js
+++ site-config.js
@@ -2,6 +2,6 @@ window.JF_SITE_CONFIG = {
   // Set these before deploying if you want construction mode to work globally.
   // jsonBinId: the Bin ID of your private JSONBin record
   // jsonBinReadKey: a read-only JSONBin Access Key with Bins Read permission
-  jsonBinId: 'PASTE_YOUR_JSONBIN_ID_HERE',
-  jsonBinReadKey: 'PASTE_YOUR_READ_ONLY_ACCESS_KEY_HERE'
+  jsonBinId: '6a36da9bda38895dfee3d710',
+  jsonBinReadKey: '$2a$10$D2qJ4PLoQQxOmtQAaC/l8ukd/se88HZ4FCnl4c.dLxWiEhBKaJxqW'
 };
